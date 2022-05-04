4babff98e83d01f543000359dcd8ec0404801be9
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-05-04 07:21:57 +0530

    [Manoj]Adding_All_Attributes

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -1,2 +1,17 @@
 -----UC1 CREATE ADDRESSBOOK SERVICE DATABASE-----
 create database AddressBook
+-------UC2 CREATING ADDRESS BOOK TABLE WITH FIRST LAST NAMES,ADDRESS,CITY,STATE,ZIP,PHNUM,EMAIL---
+drop table AddressBookTable;
+----
+create table AddressBookTable
+(
+personId int identity(1,1) primary key,
+FirstName varchar(100),
+LastName  varchar(100),
+Address varchar(200),
+city varchar(50),
+state varchar(100),
+zip bigint,
+PhoneNumber bigint,
+EMail varchar(50)
+)
