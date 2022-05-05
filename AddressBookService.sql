b97ba36763e68bd1a452d65693f427abac89871f
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-05-05 09:42:02 +0530

    [Manoj]Adding_ERDiagram

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -37,14 +37,58 @@ select * from AddressBookTable where city='NLR' order by(firstName)
 select * from AddressBookTable where state='K.N' order by(firstName)
 --UC9-Adding type of the contact
 alter table addressBookTable add bookType varchar(200)
-update AddressBookTable set bookType='Family' where personId=1 or personId=6
+update AddressBookTable set bookType='Family' where personId=1 or personId=2
 update AddressBookTable set bookType='Friend' where personId=4 or personId=3
 update AddressBookTable set bookType='Profession' where personId=7
+select * from AddressBookTable
 --UC10 Calculating the size of the record based on type
 select count(phoneNumber)as NoOfContact,
 bookType from AddressBookTable group by bookType
 select * from AddressBookTable
 --UC11 Adding contact in Two type
-insert into AddressBookTable values ('Vijay','Kumar','Anna Nagar','Kottaiyam','Kerala',869456,9654973250,'vk@gmail.com','Friend'),
-insert into AddressBookTable values ('John','Kumar','Lisbon','portugal','Europe',540021,351210205168,'lis@gmail.com','Family')
+insert into AddressBookTable values ('Vijay','Kumar','Anna Nagar','Kottaiyam','Kerala',869456,9654973250,'vk@gmail.com','Friend')
 select * from AddressBookTable
+--UC12 Creating the ER Diagram for the Table------
+drop table ContactType
+Create table ContactType 
+(
+ typeId int primary key,
+ typeName varchar(50)
+)
+drop table ContactAddress
+create table ContactAddress
+(
+ contactId int ,
+ typeId int
+)
+drop table AddressBook 
+create table AddressBook
+(
+  personId int identity(1,1) primary key,
+  firstName varchar(100),
+  lastName varchar(100),
+  address varchar(200),
+  city varchar(50),
+  state varchar(100),
+  zip bigint,
+  phoneNumber bigint,
+  email varchar(50),
+  typeId int
+)
+--inserting records into the AddressBook----------
+Insert into AddressBook (firstName,lastName,address,city,state,zip,phoneNumber,email)
+Select firstName,lastName,address,city,state,zip,phoneNumber,email from AddressBookTable
+
+Alter table AddressBook drop column typeId
+select * from AddressBook
+
+--Inserting the contact type such as Friends and family, Profession
+insert into ContactType values (1,'Friend'),(2,'Family'),(3,'Profession')
+select * from ContactType
+
+--Inserting the Contact Address that link type and person id
+insert into ContactAddress values(2,2),(3,2),(2,1),(4,1),(5,3)
+select * from ContactAddress
+Alter table ContactAddress Add foreign key (contactId) REFERENCES AddressBook(personId)
+Alter table ContactAddress Add foreign key (typeId) REFERENCES ContactType(typeId)
+
